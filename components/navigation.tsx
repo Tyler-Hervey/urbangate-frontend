39197720afdf3d6729324bd80b83ee72f6831bef
Tyler Hervey <hervey711@gmail.com>
2023-12-18 19:11:27 -0600

    initial commit

---
+++ components/navigation.tsx
@@ -0,0 +1,16 @@
+import react from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+const Navigation = () => {
+  return (
+    <section>
+      <div>
+        <Image src='' alt='Vercel Logo' width={72} height={16} />
+      </div>
+      <Link href='/'>Home</Link>
+      <Link href='/about'>About</Link>
+    </section>
+  );
+};
+
+export default Navigation;
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1 className='text-5xl font-bold'>UrbanGate</h1>
+      <h1 className='text-5xl font-bold'>About</h1>
     </main>
   );
 }
